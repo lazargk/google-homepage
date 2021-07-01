295cc831690290201568a5fe1294902ec372dcda
lazargk <gumarov.lazar@gmail.com>
2021-07-02 00:33:02 +0500

    init: create folders

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    display:flex;
+    flex-direction: column;
+`
+
+const Home = () => {
+    return (
+        <Container>
+            <h1>home</h1>
+        </Container>
+    )
+}
+
+export default Home
