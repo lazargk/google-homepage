593a563ca991bb19f2d55a347c998976e3310ffd
Lazar Gumarov <54982041+lazargk@users.noreply.github.com>
2021-07-02 09:48:11 +0500

    init: Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+
+const Nav = styled.nav``;
+const LeftLinks = styled.div``;
+const RightLinks = styled.div``;
+const NavLinks = styled(Link)``;
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <LeftLinks>
+        <NavLinks to="/about">About</NavLinks>
+        <NavLinks to="/store">Store</NavLinks>
+      </LeftLinks>
+      <RightLinks>
+        <NavLinks to="/gmail">Gmail</NavLinks>
+        <NavLinks to="/images">Images</NavLinks>
+      </RightLinks>
+    </Nav>
+  );
+};
+
+export default Navbar;
